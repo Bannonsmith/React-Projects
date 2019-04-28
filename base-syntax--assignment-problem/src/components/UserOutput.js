6a400fa6a6dba9a792998cb782d5b78a4275d8d7
Bannonsmith <Bannonsmith13@gmail.com>
2019-04-28 11:00:40 -0500

    small assignment using props and state in React

---
+++ base-syntax--assignment-problem/src/components/UserOutput.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserOutput = (props) => {
+        return (
+            <div>
+                <h1>username: {props.name}</h1>
+                <p>lorem ipsoum</p>
+                <p>lorem ipsoum</p>
+            </div>
+        );
+    
+}
+
+export default UserOutput;
